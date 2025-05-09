0b29d1fa8bba4ff5e3a269483dabb0c84adb469e
NguyenBaoSonTLU <fancr7silver@gmail.com>
2025-05-09 16:28:11 +0700

    Xóa thư mục Bai2

--- Bai2/Code/InventoryLogger.java
+++
@@ -1,7 +0,0 @@
-// InventoryLogger.java
-public class InventoryLogger {
-    public void logInventoryChange(Product product) {
-        System.out.println("San pham " + product.getProductId() +
-                           " co so luong: " + product.getQuantity());
-    }
-}
